2101f3c6becc5936e85c42d33623942ba34fcf54
Gabriel Lorenzo <teteruga.lorenzo@gmail.com>
2021-09-02 17:40:52 -0300

    Removed Gerdau email validation

--- js/validator.js
+++ js/validator.js
@@ -1,41 +1,43 @@
 /** @format */
 
 let email_domains = [
-  "@gerdau.com",
-  "@gerdau.com.br",
-  "@gerdau.com.co",
-  "@gerdaumetaldom.com",
-  "@siderperu.com.pe",
-  "@sizuca.com.ve",
-  "@gerdau.com.uy",
-  "@sider.com.pe",
-  "@gerdau.com.ar",
-  "@gerdaudiaco.com",
-  "@gerdaucorsa.com.mx",
+    "@gerdau.com",
+    "@gerdau.com.br",
+    "@gerdau.com.co",
+    "@gerdaumetaldom.com",
+    "@siderperu.com.pe",
+    "@sizuca.com.ve",
+    "@gerdau.com.uy",
+    "@sider.com.pe",
+    "@gerdau.com.ar",
+    "@gerdaudiaco.com",
+    "@gerdaucorsa.com.mx",
 ];
 
 function validate() {
-  let form = document.querySelector("form");
-  let form_data = new FormData(form);
+    let form = document.querySelector("form");
+    let form_data = new FormData(form);
 
-  validateEmail(form_data.get("email"));
+    // validateEmail(form_data.get("email"));
 
-  if (!form.checkValidity()) {
-    form.reportValidity();
-    return false;
-  }
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        return false;
+    }
 
-  return true;
+    return true;
 }
 
 function validateEmail(email) {
-  if (
-    !email_domains.includes(email.substring(email.indexOf("@"), email.length))
-  ) {
-    document
-      .querySelector("#email_input")
-      .setCustomValidity("O email precisa ser da Gerdau.");
-  } else {
-    document.querySelector("#email_input").setCustomValidity("");
-  }
+    if (
+        !email_domains.includes(
+            email.substring(email.indexOf("@"), email.length)
+        )
+    ) {
+        document
+            .querySelector("#email_input")
+            .setCustomValidity("O email precisa ser da Gerdau.");
+    } else {
+        document.querySelector("#email_input").setCustomValidity("");
+    }
 }
